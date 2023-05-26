9cd46a9ffef6eef1225b2338de0b0bad4ea9753b
MJ <129634010+mayaj0yce@users.noreply.github.com>
2023-05-25 21:29:41 -0600

    worked on search button and api, formatted css (#29)
    Co-authored-by: Maridon Romney <mkromney@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,24 @@ var requestUrl = 'https://pokeapi.co/api/v2/ability/{id or name}/';
 
 
 
+// Maridon's Search Bar: 
+
+// Function searching the API: 
+
+function searchAPI(pokeName) {
+  var url = "https://pokeapi.co/api/v2/pokemon/{id or name}/";
+
+  fetch(url).then(function(response){
+    return response.json() 
+    
+    })
+        
+  .then(function(data){
+    //Sets a variable that clears the data when called below. //
+    clear#######()
+
+    console.log(data)
+    
+
+}}
+  
