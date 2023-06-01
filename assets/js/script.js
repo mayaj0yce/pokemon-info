17d9f9a7888ab24b7755245dbf506397f0b9d9fe
Maridon Romney <mkromney@gmail.com>
2023-05-31 22:31:23 -0600

    Added additional API pathways, trying to integrate, minimal functionality. Incomplete integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-async function getPokemonInfo(pokemon) {
-  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+/////////////////////////////////////////////////////////////////////////////////
+async function getPokemonNameInfo(pokemon) {
+  const apiNameUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiNameUrl);
     const data = await response.json();
 
     // Extracts the relevant information from the data object. //MR 31.05.23
     const pokemonName = data.name;
     const pokemonAbilities = data.abilities.map(ability => ability.ability.name);
-    const pokemonMoves = data.moves.map(move => move.move.name);
+    
 
     // Return the extracted information - ***ISSUE***, only accepts all lower case pokemon names, need to make sure it accepts and prints the pokemon name's first letter. //MR 31.05.23
     return {
       name: pokemonName,
       abilities: pokemonAbilities,
-      moves: pokemonMoves
+      
+      
     };
   } catch (error) {
     // Handle any errors that occur during the API request. //MR 31.05.23
@@ -23,13 +25,63 @@ async function getPokemonInfo(pokemon) {
   }
 }
 
+
+////////////////////////////////POKE ID URL:
+
+async function getPokemonIdInfo(id) {
+  const apiIdUrl = `https://pokeapi.co/api/v2/characteristic/id/${id}`;
+
+  try {
+    const response = await fetch(apiIdUrl);
+    const data = await response.json();
+
+    const pokemonId = data.id;
+    
+    return {
+      id: pokemonId,
+      
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+
+}
+
+ ///////////////////////////POKEMON ENCOUNTER LOCATION**********
+
+ async function getPokemonLocInfo(encounter) {
+  const apiEncounterUrl = `https://pokeapi.co/api/v2/pokemon/encounter/${encounter}`;
+
+  try {
+    const response = await fetch(apiEncounterUrl);
+    const data = await response.json();
+
+    // Extracts the relevant information from the data object. //MR 31.05.23
+    const pokemonEncounter = data.encounter;
+    
+
+    // Return the extracted information - ***ISSUE***, only accepts all lower case pokemon names, need to make sure it accepts and prints the pokemon name's first letter. //MR 31.05.23
+    return {
+      encounter: pokemonEncounter,
+      
+    };
+  } catch (error) {
+    // Handle any errors that occur during the API request. //MR 31.05.23
+    console.error('Error:', error);
+    return null;
+  }
+}
+////////////////////////////////////////////////////////////////////////////
+//YOU'LL PROBABLY HAVE TO TRIPLICATE THE CODE BELOW AS WELL.
+
 // Function to handle the search button click event. //MR 31.05.23
 function handleSearch() {
   const pokeInput = document.getElementById('pokeInput');
   const searchTerm = pokeInput.value.trim();
 
   // Calls the API function with the relevant search term. This creates a container below the search bar that needs to be moved into MJ's section //MR 31.05.23
-  getPokemonInfo(searchTerm)
+  getPokemonNameInfo(searchTerm)
     .then(result => {
       const resultContainer = document.getElementById('resultContainer');
 
@@ -41,15 +93,78 @@ function handleSearch() {
         const pokemonName = document.createElement('h3');
         pokemonName.textContent = `Name: ${result.name}`;
 
-        const abilitiesList = document.createElement('p');
-        abilitiesList.textContent = `Abilities: ${result.abilities.join(', ')}`;
-
-        const movesList = document.createElement('p');
-        movesList.textContent = `Moves: ${result.moves.join(', ')}`;
+        const pokemonEncounter = document.createElement('p');
+        abilitiesList.textContent = `Where to encounter: ${result.pokemonEncounter.join(', ')}`;
 
         resultContainer.appendChild(pokemonName);
         resultContainer.appendChild(abilitiesList);
-        resultContainer.appendChild(movesList);
+        
+      } else {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'No Pokémon found with the provided ID or name.';
+        resultContainer.appendChild(errorMessage);
+      }
+    });
+
+    getPokemonIdInfo(searchTerm)
+    .then(result => {
+      const resultContainer = document.getElementById('resultContainer');
+
+   // Clears the searchButton's input field. //MR 31.05.23
+  pokeInput.value = '';
+
+      if (result) {
+        // Creates and appends elements in order to display the Pokémon information. //MR 31.05.23
+        const pokemonName = document.createElement('h3');
+        pokemonName.textContent = `Name: ${result.name}`;
+
+        const pokemonId = document.createElement('h3');
+        pokemonId.textContent = `Poke Id: ${result.id}`;
+
+        const pokemonEncounter = document.createElement('p');
+        abilitiesList.textContent = `Where to encounter: ${result.pokemonEncounter.join(', ')}`;
+
+        const abilitiesList = document.createElement('p');
+        abilitiesList.textContent = `Abilities: ${result.abilities.join(', ')}`;
+
+        resultContainer.appendChild(pokemonName);
+        resultContainer.appendChild(pokemonId);
+        resultContainer.appendChild(pokemonEncounter);
+        resultContainer.appendChild(abilitiesList);
+        
+      } else {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'No Pokémon found with the provided ID or name.';
+        resultContainer.appendChild(errorMessage);
+      }
+    });
+
+    getPokemonLocInfo(searchTerm)
+    .then(result => {
+      const resultContainer = document.getElementById('resultContainer');
+
+      // Clears the searchButton's input field. //MR 31.05.23
+  pokeInput.value = '';
+
+      if (result) {
+        // Creates and appends elements in order to display the Pokémon information. //MR 31.05.23
+        const pokemonName = document.createElement('h3');
+        pokemonName.textContent = `Name: ${result.name}`;
+
+        const pokemonId = document.createElement('h3');
+        pokemonId.textContent = `Poke Id: ${result.id}`;
+
+        const pokemonEncounter = document.createElement('p');
+        abilitiesList.textContent = `Where to encounter: ${result.pokemonEncounter.join(', ')}`;
+
+        const abilitiesList = document.createElement('p');
+        abilitiesList.textContent = `Abilities: ${result.abilities.join(', ')}`;
+
+        resultContainer.appendChild(pokemonName);
+        resultContainer.appendChild(pokemonId);
+        resultContainer.appendChild(pokemonEncounter);
+        resultContainer.appendChild(abilitiesList);
+        
       } else {
         const errorMessage = document.createElement('p');
         errorMessage.textContent = 'No Pokémon found with the provided ID or name.';
