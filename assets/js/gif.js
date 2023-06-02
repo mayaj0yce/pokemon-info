6a9c0b5b16fd8c43ad15f25a8ddb9e27239e631b
mayaj0yce <m.j.thayne@gmail.com>
2023-06-01 21:31:28 -0600

    load

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,7 +1,7 @@
 // const image = 'https:' + $('a[href*="/wiki/File:"] > img').attr("src");
 
 // // 7964debe-a160-40e6-8cc3-7f21841c780f api key
-// var  https://api.pokemontcg.io/v2/cards{APIKEY}
+// var requestUrl2 =  https://api.pokemontcg.io/v2/cards{APIKEY}
 // function gifImage() {
 // fetch (requestUrl2)
 
